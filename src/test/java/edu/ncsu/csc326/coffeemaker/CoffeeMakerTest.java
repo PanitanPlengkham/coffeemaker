4d7ffaaf146cfeecfac07f9edb04bd808a7ef456
PanitanPlengkham <panitan.pl@ku.th>
2021-09-07 16:28:33 +0700

    Add jacoco coverage

--- src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -18,7 +18,7 @@
  */
 package edu.ncsu.csc326.coffeemaker;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -29,7 +29,7 @@ import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 /**
  * Unit tests for CoffeeMaker class.
  * 
- * @author Sarah Heckman
+ * @author Panitan Plengkham
  */
 public class CoffeeMakerTest {
 	
@@ -91,27 +91,30 @@ public class CoffeeMakerTest {
 		recipe4.setAmtSugar("1");
 		recipe4.setPrice("65");
 	}
-	
-	
+
+
 	/**
 	 * Given a coffee maker with the default inventory
 	 * When we add inventory with well-formed quantities
 	 * Then we do not get an exception trying to read the inventory quantities.
-	 * 
+	 *
 	 * @throws InventoryException  if there was an error parsing the quanity
 	 * 		to a positive integer.
 	 */
 	@Test
 	public void testAddInventory() throws InventoryException {
 		coffeeMaker.addInventory("4","7","0","9");
+		coffeeMaker.addInventory("1","2","20","1");
+		// When sugar more than 0 it will fail to add inventory
 	}
-	
+
+
 	/**
 	 * Given a coffee maker with the default inventory
-	 * When we add inventory with malformed quantities (i.e., a negative 
+	 * When we add inventory with malformed quantities (i.e., a negative
 	 * quantity and a non-numeric string)
 	 * Then we get an inventory exception
-	 * 
+	 *
 	 * @throws InventoryException  if there was an error parsing the quanity
 	 * 		to a positive integer.
 	 */
@@ -119,10 +122,91 @@ public class CoffeeMakerTest {
 	public void testAddInventoryException() throws InventoryException {
 		coffeeMaker.addInventory("4", "-1", "asdf", "3");
 	}
-	
+
+	/**
+	 * When check the inventory it will display with the string
+	 * So all item in the inventory should equal to 15 when started
+	 */
+	@Test
+	public void testCheckInventory() {
+		assertEquals(coffeeMaker.checkInventory(),"Coffee: 15\nMilk: 15\nSugar: 15\nChocolate: 15\n");
+	}
+
+	/**
+	 * When we make a coffee, if there is not enough inventory to make a coffee
+	 * Then we will get our money back.
+	 */
+	@Test
+	public void testNotEnoughInventory() {
+		coffeeMaker.addRecipe(recipe2);
+		int change = coffeeMaker.makeCoffee(0, 75);
+		assertEquals(75, change);
+	}
+
+	/**
+	 * Test adding the recipe that is valid or not.
+	 */
+	@Test
+	public void testAddRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(recipe1, coffeeMaker.getRecipes()[0]);
+	}
+
+	/**
+	 * The recipe should not be added
+	 * When we add a recipe over three.
+	 */
+	@Test
+	public void testAddRecipeOverThree() {
+		assertTrue(coffeeMaker.addRecipe(recipe1));
+		assertTrue(coffeeMaker.addRecipe(recipe2));
+		assertTrue(coffeeMaker.addRecipe(recipe3));
+		assertFalse(coffeeMaker.addRecipe(recipe4));
+	}
+
+	/**
+	 * Test add a recipe which is already exist
+	 * and the recipe should not be added
+	 */
+	@Test
+	public void testAddDuplicateRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		assertFalse(coffeeMaker.addRecipe(recipe1));
+	}
+
+	/**
+	 * Add a recipe and delete it by the index
+	 */
+	@Test
+	public void testDeleteRecipe() {
+		coffeeMaker.addRecipe(recipe2);
+		coffeeMaker.deleteRecipe(0);
+		assertNotEquals(recipe2, coffeeMaker.getRecipes()[0]);
+		assertNull(coffeeMaker.getRecipes()[0]);
+	}
+
+	/**
+	 * Deleting a recipe that not added yet so it won't be exist
+	 */
+	@Test
+	public void testDeleteRecipeNotAddedRecipe() {
+		assertNull(coffeeMaker.deleteRecipe(0));
+	}
+
+
+	/**
+	 * Test add a recipe and then edit it
+	 */
+	@Test
+	public void testEditRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(recipe1.getName(), coffeeMaker.editRecipe(0, recipe2));
+		assertEquals(recipe2, coffeeMaker.getRecipes()[0]);
+	}
+
 	/**
 	 * Given a coffee maker with one valid recipe
-	 * When we make coffee, selecting the valid recipe and paying more than 
+	 * When we make coffee, selecting the valid recipe and paying more than
 	 * 		the coffee costs
 	 * Then we get the correct change back.
 	 */
@@ -130,6 +214,69 @@ public class CoffeeMakerTest {
 	public void testMakeCoffee() {
 		coffeeMaker.addRecipe(recipe1);
 		assertEquals(25, coffeeMaker.makeCoffee(0, 75));
+	}
+
+	/**
+	 * When we make a coffee, select the valid recipe and paying less than the coffee costs
+	 * Then we will get our money back.
+	 */
+	@Test
+	public void testPaidLessThanCoffeeCost() {
+		coffeeMaker.addRecipe(recipe2);
+		assertEquals(50, coffeeMaker.makeCoffee(0, 50));
+	}
+
+	/**
+	 * When we make a coffee, select the valid recipe and paying equal to the coffee costs
+	 * Then we will not get any change.
+	 */
+	@Test
+	public void testPaidEqualToCoffeeCost() {
+		coffeeMaker.addRecipe(recipe1);
+		assertEquals(0, coffeeMaker.makeCoffee(0, 50));
+	}
+
+	/**
+	 * When we make a coffee, select the valid recipe and paying more than the coffee costs
+	 * Then we will get a change.
+	 */
+	@Test
+	public void testPaidMoreThanCoffeeCost() {
+		coffeeMaker.addRecipe(recipe3);
+		assertEquals(20, coffeeMaker.makeCoffee(0, 120));
+	}
+
+	/**
+	 * Make a coffee with negative index
+	 * makeCoffee() should handle negative argument and not make any error, then return something.
+	 *
+	 */
+	@Test
+	public void testMakeCoffeeWithNegativeIndexRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		int change = coffeeMaker.makeCoffee(-1, 100);
+		assertEquals(100, change);
+	}
+
+	/**
+	 * Make a coffee with an invalid index (Out of bound)
+	 * makeCoffee() should handle out of bound argument and not make any error, then return something.
+	 */
+	@Test
+	public void testMakeCoffeeWithOutOfBoundIndexRecipe() {
+		coffeeMaker.addRecipe(recipe1);
+		int change = coffeeMaker.makeCoffee(10, 100);
+		assertEquals(100, change);
+	}
+
+	/**
+	 * Make a coffee with null recipe
+	 * Then we get our money back.
+	 */
+	@Test
+	public void testMakeCoffeeWithNullRecipe() {
+		int change = coffeeMaker.makeCoffee(0, 100);
+		assertEquals(100, change);
 	}
 
 }
